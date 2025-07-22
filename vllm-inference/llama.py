a8402dcf88b08b0e9f31f2592080a3def136dbd8
Paul Yang <paul@run.house>
2025-07-22 12:19:51 -0400

    add

--- vllm-inference/llama.py
+++ vllm-inference/llama.py
@@ -1,10 +1,22 @@
-# ### Load RAG LLM Inference Service
-# Deploy an open LLM, Llama 3 in this case, to 1 or more GPUs in the cloud.
-# We will use vLLM to serve the model due to it's high performance.
-#
+# ## Hello World: Deploy an LLM Inference Service
+# Our inference `Hello, World` has you deploying an open LLM (Llama 3 in this case) to 1 or more GPUs in the cloud,
+# using vLLM to serve the model due to it's high performance. All you have to do is call `kt deploy llama.py` to
+# deploy the service, and then you can run `python llama.py` to see how you can call the remote service from
+# your local machine.
+
 import kubetorch as kt
 
 
+# ### Decorate a Regular Inference Class
+# We start with a regular inference class called `LlamaModel` which has a `generate()` method that runs inference with vLLM.
+# Then, we apply our decorators which will allow this service to be deployed with `kt deploy.` In the `kt.compute` decorator,
+# we request 1 GPU for each replica of the inference class; our cluster is configured with an L4, but you can easily
+# specify the exact compute requirements here, including a specific GPU type, CPU, memory, etc. We also specify that we want
+# it to start with a public PyTorch image with vllm installed additionally; you can launch the service with your team's base
+# Docker image, but optionally (especially during development iteration) lightly modify that image without having to rebuild
+# that image fully. We also use the `kt.autoscale` decorator to specify the autoscale conditions for this service ; this service will
+# scale to 0, have a maximum scale of 5, and autoscale up when there are 100 concurrent requests. There are many more
+# flexible options you have here as well.
 @kt.compute(
     gpus="1",
     image=kt.Image(image_id="nvcr.io/nvidia/pytorch:24.08-py3").run_bash(
@@ -14,7 +26,7 @@ import kubetorch as kt
     launch_timeout=1200,  # Need more time to load the model
     secrets=["huggingface"],
 )
-@kt.autoscale(initial_replicas=1, min_replicas=0, max_replicas=4, concurrency=1000)
+@kt.autoscale(initial_scale=1, min_scale=0, max_scale=5, concurrency=100)
 class LlamaModel:
     def __init__(self, model_id="meta-llama/Meta-Llama-3-8B-Instruct"):
         from vllm import LLM
@@ -30,7 +42,6 @@ class LlamaModel:
     def generate(
         self, queries, temperature=0.65, top_p=0.95, max_tokens=5120, min_tokens=32
     ):
-        """Generate text with proper error handling and model loading"""
         from vllm import SamplingParams
 
         sampling_params = SamplingParams(
@@ -44,8 +55,11 @@ class LlamaModel:
         return [output.outputs[0].text for output in req_output]
 
 
-# Uncomment to test the LlamaModel service locally.
+# ### Call the Inference
+# Once we call `kt deploy llama.py,` an autoscaling service is stood up that we can use directly in our programs.
+# We are calling the service in our script here, but you could identically call the service from within your FastAPI app,
+# an orchestrator for batch inference, or anywhere else.
 if __name__ == "__main__":
-    # LlamaModel.deploy()
+    # LlamaModel.deploy() # Uncomment to test the LlamaModel service locally.
     res = LlamaModel.generate(["Who is Randy Jackson?"], max_tokens=100)
     print(res)
