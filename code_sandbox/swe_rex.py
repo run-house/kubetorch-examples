6f31dcb03d3309951c78490c8786cddc072c687c
Paul Yang <paul@run.house>
2025-08-08 16:03:02 -0400

    lint swe rex

---
+++ code_sandbox/swe_rex.py
@@ -0,0 +1,30 @@
+import kubetorch as kt
+
+from swerex.runtime.local import Command, LocalRuntime
+
+
+def get_sandbox(name: str):
+    cpus = kt.Compute(
+        cpus="4",
+        image=kt.Image().pip_install(["swe-rex"]),
+        allowed_serialization=["pickle"],
+    )
+    runtime = kt.cls(LocalRuntime, name=name).to(cpus)
+    runtime.serialization = "pickle"
+    return runtime
+
+
+if __name__ == "__main__":
+    sandbox = get_sandbox("swe-rex-sandbox-1")
+    # Check if the runtime is alive
+    is_alive_response = sandbox.is_alive()
+    print(f"Is the runtime alive? {is_alive_response.is_alive}")
+
+    # echo a message to the runtime
+    echo_response = sandbox.execute(
+        Command(command="echo 'Hello, Swerex!'", shell=True)
+    )
+
+    # Close the runtime when done
+    close_response = sandbox.close()
+    print("Runtime closed.")
